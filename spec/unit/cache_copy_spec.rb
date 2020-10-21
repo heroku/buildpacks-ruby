9295f5fa3df73a86818fdcb2eb2e8ae3fb16ee29
Richard Schneeman <rschneeman@salesforce.com>
2020-10-21 14:42:30 -0500

    Fix V2 Indentation

--- spec/unit/cache_copy_spec.rb
+++ spec/unit/cache_copy_spec.rb
@@ -7,20 +7,19 @@ RSpec.describe "cache copy" do
       dest_dir = dir.join("foo").tap(&:mkpath)
       cache_dir = dir.join("cache").tap(&:mkpath)
 
-      expect(dest_dir.entries.map(&:to_s)).to eq([".", ".."])
-
+      expect(dest_dir).to be_empty
       HerokuBuildpackRuby::CacheCopy.new(cache_dir: cache_dir, dest_dir: dest_dir).call do |block_value|
         dest_dir.join("hello.txt").write "hello world"
+
         expect(dest_dir.entries.map(&:to_s)).to include("hello.txt")
         expect(dest_dir).to eq(block_value)
       end
-
       expect(dest_dir.entries.map(&:to_s)).to include("hello.txt")
       expect(cache_dir.entries.map(&:to_s)).to include("hello.txt")
 
       different_dest_dir = dir.join("bar").tap(&:mkpath)
-      expect(different_dest_dir.entries.map(&:to_s)).to_not include("hello.txt")
 
+      expect(different_dest_dir).to be_empty
       HerokuBuildpackRuby::CacheCopy.new(cache_dir: cache_dir, dest_dir: different_dest_dir).call do
         expect(different_dest_dir.entries.map(&:to_s)).to include("hello.txt")
       end

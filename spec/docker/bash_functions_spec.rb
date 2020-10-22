af38dc1b4825bc4f48a07aff8bdb399e26ea972c
Richard Schneeman <rschneeman@salesforce.com>
2020-10-22 15:53:28 -0500

    Debugging

--- spec/docker/bash_functions_spec.rb
+++ spec/docker/bash_functions_spec.rb
@@ -2,45 +2,43 @@ require_relative "../spec_helper.rb"
 
 RSpec.describe "bash_functions.sh that need docker" do
   it "compiles node apps" do
-    Hatchet::Runner.new("minimal_webpacker").tap do |app|
-      app.in_directory do
-        contents = <<~EOM
-          #! /usr/bin/env bash
-          set -eu
+    Hatchet::Runner.new("minimal_webpacker").in_directory do
+      contents = <<~EOM
+        #! /usr/bin/env bash
+        set -eu
+        export STACK="heroku-18"
 
-          cd /myapp
-          export STACK="heroku-18"
+        #{bash_functions_file.read}
 
-          #{bash_functions_file.read}
 
-          build_dir="$PWD"
-          cache_dir="/tmp/cache_dir"
-          env_dir="/tmp/env_dir"
+        build_dir="/tmp/build_dir"
+        cache_dir="/tmp/cache_dir"
+        env_dir="/tmp/env_dir"
 
-          mkdir -p "$cache_dir"
-          mkdir -p "$env_dir"
+        mkdir -p "$cache_dir"
+        mkdir -p "$env_dir"
+        mkdir -p "$build_dir"
+        cp -r /myapp/* "$build_dir"
 
-          compile_buildpack_v2 "$build_dir" "$cache_dir" "$env_dir" "https://buildpack-registry.s3.amazonaws.com/buildpacks/heroku/nodejs.tgz" "heroku/nodejs"
+        compile_buildpack_v2 "$build_dir" "$cache_dir" "$env_dir" "https://buildpack-registry.s3.amazonaws.com/buildpacks/heroku/nodejs.tgz" "heroku/nodejs"
 
-          echo "which node $(which node)"
-          echo "which yarn $(which yarn)"
-        EOM
+        echo "which node $(which node)"
+        echo "which yarn $(which yarn)"
+      EOM
 
-        script = Pathname.new(".").join("script.sh")
-        script.write(contents)
-        FileUtils.chmod("+x", script)
+      script = Pathname.new(".").join("script.sh")
+      script.write(contents)
+      FileUtils.chmod("+x", script)
 
-        output = `docker run -v "$PWD:/myapp" -it --rm heroku/heroku:18-build /myapp/script.sh 2>&1`
+      output = `docker run -v "$PWD:/myapp" -it --rm heroku/heroku:18-build /myapp/script.sh 2>&1`
+      expect(output).to include("Build succeeded")
+      expect(output).to include("installing node")
+      expect(output).to include("installing yarn")
 
-        expect(output).to include("Build succeeded")
-        expect(output).to include("installing node")
-        expect(output).to include("installing yarn")
+      expect(output).to include("which node /tmp/build_dir/.heroku/node/bin/node")
+      expect(output).to include("which yarn /tmp/build_dir/.heroku/yarn/bin/yarn")
 
-        expect(output).to include("which node /myapp/.heroku/node/bin/node")
-        expect(output).to include("which yarn /myapp/.heroku/yarn/bin/yarn")
-
-        expect($?.success?).to be_truthy
-      end
+      expect($?.success?).to be_truthy
     end
   end
 end

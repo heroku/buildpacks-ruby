452c56595bfba85677861607141aabdf10421c51
Richard Schneeman <rschneeman@salesforce.com>
2021-03-17 15:49:13 -0500

    Abort build on `rake assets:precompile` failure
    When `rake assets:precompile` fails, raise a helpful error.

--- spec/unit/assets_precompile_spec.rb
+++ spec/unit/assets_precompile_spec.rb
@@ -34,6 +34,48 @@ module HerokuBuildpackRuby
       end
     end
 
+    it "fails gracefully" do
+      My::Pathname.mktmpdir do |dir|
+        Dir.chdir(dir) do
+          dir.join("Rakefile").write <<~EOM
+            task "assets:precompile" do
+              raise "Nope"
+            end
+
+            task "assets:clean" do
+              puts "woop woop clean worked"
+            end
+          EOM
+
+          user_comms = UserComms::Null.new
+          assets_precompile = AssetsPrecompile.new(
+            has_assets_precompile: true,
+            has_assets_clean: true,
+            user_comms: user_comms,
+            app_dir: dir,
+          )
+
+          expect {
+            Bundler.with_original_env do
+              assets_precompile.call
+            end
+          }.to raise_error {|error|
+            expect(error).to be_a(BuildpackErrorNoBacktrace)
+            expect(error.title).to eq("Precompiling assets failed")
+            expect(error.body).to include("Nope")
+          }
+
+          # Gives specific help when database is not provisioned
+          expect {
+            assets_precompile.precompile_fail(output: "127.0.0.1")
+          }.to raise_error {|error|
+            expect(error).to be_a(BuildpackErrorNoBacktrace)
+            expect(error.link).to eq("https://devcenter.heroku.com/articles/pre-provision-database")
+          }
+        end
+      end
+    end
+
     it "skips asset compilation when manifest is found" do
       My::Pathname.mktmpdir do |dir|
         public_dir = dir.join("public/assets").tap(&:mkpath)

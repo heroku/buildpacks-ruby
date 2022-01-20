a24a1e0540ba61bfa4c63d49bf3970337c42339a
Richard Schneeman <rschneeman@salesforce.com>
2022-01-20 14:23:51 -0600

    Inject Ruby version from main

--- src/layers/ruby.rs
+++ src/layers/ruby.rs
@@ -11,7 +11,11 @@ use libcnb::data::layer_content_metadata::LayerTypes;
 use libcnb::generic::GenericMetadata;
 use libcnb::layer::{Layer, LayerResult, LayerResultBuilder};
 
-pub struct RubyLayer;
+use libcnb::data::buildpack::StackId;
+
+pub struct RubyLayer {
+    pub version: String,
+}
 
 use url::Url;
 
@@ -29,19 +33,16 @@ impl Layer for RubyLayer {
 
     fn create(
         &self,
-        _context: &BuildContext<Self::Buildpack>,
+        context: &BuildContext<Self::Buildpack>,
         layer_path: &Path,
     ) -> Result<LayerResult<Self::Metadata>, RubyBuildpackError> {
-        let stack = "heroku-20";
-        let version = "2.7.4";
-
-        println!("---> Download and extracting Ruby {}", version);
+        println!("---> Download and extracting Ruby {}", &self.version);
 
         let tmp_ruby_tgz =
             NamedTempFile::new().map_err(RubyBuildpackError::CouldNotCreateTemporaryFile)?;
 
-        let url =
-            RubyLayer::download_url(stack, version).map_err(RubyBuildpackError::UrlParseError)?;
+        let url = RubyLayer::download_url(&context.stack_id, &self.version)
+            .map_err(RubyBuildpackError::UrlParseError)?;
 
         util::download(url.as_ref(), tmp_ruby_tgz.path())
             .map_err(RubyBuildpackError::RubyDownloadError)?;
@@ -54,17 +55,14 @@ impl Layer for RubyLayer {
 }
 
 impl RubyLayer {
-    fn download_url(
-        stack: impl AsRef<str>,
-        version: impl AsRef<str> + std::fmt::Display,
-    ) -> Result<Url, UrlError> {
+    fn download_url(stack: &StackId, version: impl std::fmt::Display) -> Result<Url, UrlError> {
         let filename = format!("ruby-{}.tgz", version);
         let base = "https://heroku-buildpack-ruby.s3.amazonaws.com";
         let mut url = Url::parse(base).map_err(UrlError::UrlParseError)?;
 
         url.path_segments_mut()
             .map_err(|_| UrlError::InvalidBaseUrl(String::from(base)))?
-            .push(stack.as_ref())
+            .push(stack)
             .push(&filename);
         Ok(url)
     }
@@ -72,11 +70,13 @@ impl RubyLayer {
 
 #[cfg(test)]
 mod tests {
+    use libcnb::data::stack_id;
+
     use super::*;
 
     #[test]
     fn test_ruby_url() {
-        let out = RubyLayer::download_url("heroku-20", "2.7.4").unwrap();
+        let out = RubyLayer::download_url(&stack_id!("heroku-20"), "2.7.4").unwrap();
         assert_eq!(
             out.as_ref(),
             "https://heroku-buildpack-ruby.s3.amazonaws.com/heroku-20/ruby-2.7.4.tgz",

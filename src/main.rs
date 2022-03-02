73b6d04ace83afe178bccfc7ba38c21e2d4eb02d
Richard Schneeman <rschneeman@salesforce.com>
2022-03-02 11:03:23 -0600

    Refactor Gemfile logic to it's own file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use crate::gemfile_lock::{BundlerVersion, GemfileLock, GemfileLockError, RubyVersion};
 use crate::layers::{BundlerLayer, RubyLayer};
 use libcnb::build::{BuildContext, BuildResult, BuildResultBuilder};
 use libcnb::data::launch::{Launch, Process};
@@ -7,67 +8,17 @@ use libcnb::generic::GenericPlatform;
 use libcnb::layer_env::Scope;
 use libcnb::{buildpack_main, Buildpack, Env};
 
+use core::str::FromStr;
+
 use crate::util::{DownloadError, UntarError, UrlError};
 use serde::Deserialize;
 use std::process::ExitStatus;
 
+mod gemfile_lock;
 mod layers;
 mod util;
 
 pub struct RubyBuildpack;
-use core::str::FromStr;
-
-#[derive(Debug)]
-struct BundleInfo {
-    bundler_version: BundlerVersion,
-    ruby_version: RubyVersion,
-}
-
-#[derive(Debug, PartialEq, Eq)]
-pub enum RubyVersion {
-    Explicit(String),
-    Default,
-}
-
-#[derive(Debug, PartialEq, Eq)]
-pub enum BundlerVersion {
-    Explicit(String),
-    Default,
-}
-
-#[derive(thiserror::Error, Debug, PartialEq)]
-pub enum BundleInfoError {
-    #[error("Regex error: {0}")]
-    RegexError(#[from] regex::Error),
-}
-
-use regex::Regex;
-impl FromStr for BundleInfo {
-    type Err = BundleInfoError;
-
-    fn from_str(string: &str) -> Result<Self, Self::Err> {
-        let bundled_with_re = Regex::new("BUNDLED WITH\\s   (\\d+\\.\\d+\\.\\d+)")
-            .map_err(BundleInfoError::RegexError)?;
-        let ruby_version_re = Regex::new("RUBY VERSION\\s   ruby (\\d+\\.\\d+\\.\\d+)")
-            .map_err(BundleInfoError::RegexError)?;
-
-        let bundler_version = match bundled_with_re.captures(string).and_then(|c| c.get(1)) {
-            Some(result) => BundlerVersion::Explicit(result.as_str().to_string()),
-            None => BundlerVersion::Default,
-        };
-
-        let ruby_version = match ruby_version_re.captures(string).and_then(|c| c.get(1)) {
-            Some(result) => RubyVersion::Explicit(result.as_str().to_string()),
-            None => RubyVersion::Default,
-        };
-
-        Ok(Self {
-            bundler_version,
-            ruby_version,
-        })
-    }
-}
-
 impl Buildpack for RubyBuildpack {
     type Platform = GenericPlatform;
     type Metadata = RubyBuildpackMetadata;
@@ -85,7 +36,7 @@ impl Buildpack for RubyBuildpack {
         println!("---> Ruby Buildpack");
 
         let gemfile_lock = std::fs::read_to_string(context.app_dir.join("Gemfile.lock")).unwrap();
-        let bundle_info = BundleInfo::from_str(&gemfile_lock)
+        let bundle_info = GemfileLock::from_str(&gemfile_lock)
             .map_err(RubyBuildpackError::GemfileLockParsingError)?;
 
         let ruby_layer = context //
@@ -160,7 +111,7 @@ pub enum RubyBuildpackError {
     UrlParseError(UrlError),
 
     #[error("Error evaluating Gemfile.lock: {0}")]
-    GemfileLockParsingError(BundleInfoError),
+    GemfileLockParsingError(GemfileLockError),
 }
 impl From<RubyBuildpackError> for libcnb::Error<RubyBuildpackError> {
     fn from(error: RubyBuildpackError) -> Self {
@@ -169,51 +120,3 @@ impl From<RubyBuildpackError> for libcnb::Error<RubyBuildpackError> {
 }
 
 buildpack_main!(RubyBuildpack);
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_parse_gemfile_lock() {
-        let info = BundleInfo::from_str(
-            r#"
-GEM
-  remote: https://rubygems.org/
-  specs:
-    mini_histogram (0.3.1)
-
-PLATFORMS
-  ruby
-  x86_64-darwin-20
-  x86_64-linux
-
-DEPENDENCIES
-  mini_histogram
-
-RUBY VERSION
-   ruby 3.1.0p-1
-
-BUNDLED WITH
-   2.3.4
-"#,
-        )
-        .unwrap();
-
-        assert_eq!(
-            info.bundler_version,
-            BundlerVersion::Explicit("2.3.4".to_string())
-        );
-        assert_eq!(
-            info.ruby_version,
-            RubyVersion::Explicit("3.1.0".to_string())
-        );
-    }
-
-    #[test]
-    fn test_default_versions() {
-        let info = BundleInfo::from_str("").unwrap();
-        assert_eq!(info.bundler_version, BundlerVersion::Default);
-        assert_eq!(info.ruby_version, RubyVersion::Default);
-    }
-}

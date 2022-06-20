b95ea111a037f4961d3e539c379b6ef6cd9b7cbc
Richard Schneeman <rschneeman@salesforce.com>
2022-06-19 20:50:00 -0500

    Rename ruby layer to install_ruby_version_layer
    It's more descriptive. I also added documentation.

--- src/layers/mod.rs
+++ src/layers/mod.rs
@@ -1,9 +1,9 @@
 mod create_bundle_path_layer;
 mod download_bundler_layer;
 mod execute_bundle_install_layer;
-mod ruby;
+mod install_ruby_version_layer;
 
 pub use create_bundle_path_layer::*;
 pub use download_bundler_layer::*;
 pub use execute_bundle_install_layer::*;
-pub use ruby::*;
+pub use install_ruby_version_layer::*;

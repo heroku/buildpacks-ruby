a61e7f021caef0fe78f451c3f206533200badb75
Richard Schneeman <rschneeman@salesforce.com>
2022-10-20 17:43:56 -0500

    Hit root until boot

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,11 +1,13 @@
 #![warn(clippy::pedantic)]
 
 use libcnb_test::{
-    assert_contains, assert_empty, BuildConfig, BuildpackReference, ContainerConfig, TestRunner,
+    assert_contains, assert_empty, BuildConfig, BuildpackReference, ContainerConfig,
+    ContainerContext, TestRunner,
 };
 use std::net::SocketAddr;
 use std::time::Duration;
 use std::{io, thread};
+use ureq::Response;
 
 #[test]
 #[ignore = "integration test"]
@@ -23,19 +25,22 @@ fn test_getting_started_rails_app() {
                 context.pack_stdout,
                 r#"Running: $ BUNDLE_BIN="/layers/heroku_ruby/gems/bin" BUNDLE_CLEAN="1" BUNDLE_DEPLOYMENT="1" BUNDLE_GEMFILE="/workspace/Gemfile" BUNDLE_PATH="/layers/heroku_ruby/gems" BUNDLE_WITHOUT="development:test" bundle install"#
             );
+            println!("{}", context.pack_stdout);
 
             context.start_container(
                 ContainerConfig::new()
                     .env("PORT", TEST_PORT.to_string())
                     .expose_port(TEST_PORT),
                 |container| {
-                    thread::sleep(Duration::from_secs(5));
+                    let boot = call_root_until_boot(&container, TEST_PORT);
 
                     let server_logs = container.logs_now();
                     assert_contains!(
                         server_logs.stdout.clone() + &server_logs.stderr,
                         "Puma starting"
                     );
+
+                    boot.unwrap();
 
                     let address_on_host = container.address_for_port(TEST_PORT).unwrap();
 
@@ -57,8 +62,7 @@ fn test_default_app() {
             assert_contains!(context.pack_stdout, "---> Download and extracting Ruby");
             assert_contains!(
                 context.pack_stdout,
-                r#"Running: $ BUNDLE_BIN="/layers/heroku_ruby/gems/bin" BUNDLE_CLEAN="1" BUNDLE_DEPLOYMENT="1" BUNDLE_GEMFILE="/workspace/Gemfile" BUNDLE_PATH="/layers/heroku_ruby/gems" BUNDLE_WITHOUT="development:test" bundle install"#
-            );
+                r#"Running: $ BUNDLE_BIN="/layers/heroku_ruby/gems/bin" BUNDLE_CLEAN="1" BUNDLE_DEPLOYMENT="1" BUNDLE_GEMFILE="/workspace/Gemfile" BUNDLE_PATH="/layers/heroku_ruby/gems" BUNDLE_WITHOUT="development:test" bundle install"#);
 
             assert_contains!(context.pack_stdout, "Installing webrick");
 
@@ -71,7 +75,7 @@ fn test_default_app() {
                         .env("PORT", TEST_PORT.to_string())
                         .expose_port(TEST_PORT),
                     |container| {
-                        thread::sleep(Duration::from_secs(2));
+                        call_root_until_boot(&container, TEST_PORT).unwrap();
 
                         let server_logs = container.logs_now();
                         assert_contains!(server_logs.stderr, "WEBrick::HTTPServer#start");
@@ -90,6 +94,47 @@ fn test_default_app() {
 fn call_test_fixture_service(addr: SocketAddr) -> io::Result<String> {
     let req = ureq::get(&format!("http://{}:{}/", addr.ip(), addr.port()));
     req.call().unwrap().into_string()
+}
+
+fn call_root_until_boot(container: &ContainerContext, port: u16) -> Result<Response, ureq::Error> {
+    let mut count = 0;
+    let max_time = 10.0; //Seconds
+    let sleep = 0.1;
+
+    #[allow(clippy::cast_possible_truncation)]
+    #[allow(clippy::cast_sign_loss)]
+    let max_count = (max_time / sleep as f64).floor() as u64;
+    let mut addr = container.address_for_port(port).unwrap();
+    let mut req = ureq::get(&format!("http://{}:{}/", addr.ip(), addr.port()));
+    let mut response = req.call();
+    while count < max_count {
+        count += 1;
+        match response {
+            Err(ureq::Error::Transport(e)) => {
+                addr = container.address_for_port(port).unwrap();
+                req = ureq::get(&format!("http://{}:{}/", addr.ip(), addr.port()));
+                response = req.call();
+                println!("Waiting for connection {}, retrying in {}", e, sleep);
+            }
+            _ => break,
+        }
+
+        thread::sleep(frac_seconds(sleep));
+    }
+
+    println!(
+        "{}\n{}",
+        container.logs_now().stdout,
+        container.logs_now().stderr
+    );
+    response
+}
+
+fn frac_seconds(seconds: f64) -> Duration {
+    #[allow(clippy::cast_possible_truncation)]
+    #[allow(clippy::cast_sign_loss)]
+    let value = (seconds * 1000.0).floor() as u64;
+    Duration::from_millis(value)
 }
 
 const TEST_PORT: u16 = 1234;

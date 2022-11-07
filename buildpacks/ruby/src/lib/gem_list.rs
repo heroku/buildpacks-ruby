332f19437d2a95bf0d92118e3586c3df7b103ad4
Richard Schneeman <rschneeman@salesforce.com>
2022-11-07 17:02:21 -0600

    Move GemVersion to publicly accessible commons
    GemVersion can now be required and used independently of the RubyBuildpack

--- buildpacks/ruby/src/lib/gem_list.rs
+++ buildpacks/ruby/src/lib/gem_list.rs
@@ -2,8 +2,8 @@ use core::str::FromStr;
 use regex::Regex;
 use std::collections::HashMap;
 
-use crate::lib::gem_version::GemVersion;
 use commons::env_command::{EnvCommand, EnvCommandError};
+use commons::gem_version::GemVersion;
 use libcnb::Env;
 
 // ## Gets list of an application's dependencies

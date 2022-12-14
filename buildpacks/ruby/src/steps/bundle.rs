14ba730755c8531ab1eb13fd36bdd197e0d56961
Richard Schneeman <rschneeman@salesforce.com>
2022-12-13 18:03:25 -0600

    Remove un-needed struct for calling function

---
+++ buildpacks/ruby/src/steps/bundle.rs
@@ -0,0 +1,69 @@
+use crate::layers::{BundleInstallConfigureEnvLayer, BundleInstallDownloadBundlerLayer};
+use crate::{layers::BundleInstallCreatePathLayer, RubyBuildpack, RubyBuildpackError};
+use commons::env_command::EnvCommand;
+use commons::gemfile_lock::{ResolvedBundlerVersion, ResolvedRubyVersion};
+use libcnb::Env;
+use libcnb::{build::BuildContext, data::layer_name, layer_env::Scope};
+
+/// Primary interface for `bundle install`
+pub(crate) fn install(
+    ruby_version: ResolvedRubyVersion,
+    bundler_version: ResolvedBundlerVersion,
+    context: &BuildContext<RubyBuildpack>,
+    env: &Env,
+) -> libcnb::Result<Env, RubyBuildpackError> {
+    let mut env = env.clone();
+    // ## Setup bundler
+    //
+    // Gems will be installed here, sets BUNDLE_PATH env var
+    let create_bundle_path_layer = context.handle_layer(
+        layer_name!("gems"),
+        BundleInstallCreatePathLayer {
+            ruby_version: ruby_version.version,
+        },
+    )?;
+    env = create_bundle_path_layer.env.apply(Scope::Build, &env);
+
+    // Configures other `BUNDLE_*` settings not based on a layer path.
+    let configure_env_layer = context.handle_layer(
+        layer_name!("bundle_configure_env"),
+        BundleInstallConfigureEnvLayer,
+    )?;
+    env = configure_env_layer.env.apply(Scope::Build, &env);
+
+    // ## Download bundler
+    //
+    // Download the specified bundler version
+    let download_bundler_layer = context.handle_layer(
+        layer_name!("bundler"),
+        BundleInstallDownloadBundlerLayer {
+            version: bundler_version,
+            env: env.clone(),
+        },
+    )?;
+    env = download_bundler_layer.env.apply(Scope::Build, &env);
+
+    // ## Run `$ bundle install`
+    println!("---> Installing gems");
+    let command = EnvCommand::new_show_keys(
+        "bundle",
+        &["install"],
+        &env,
+        [
+            "BUNDLE_BIN",
+            "BUNDLE_CLEAN",
+            "BUNDLE_DEPLOYMENT",
+            "BUNDLE_GEMFILE",
+            "BUNDLE_PATH",
+            "BUNDLE_WITHOUT",
+        ],
+    );
+
+    println!("Running: $ {} ", command);
+
+    command
+        .stream()
+        .map_err(RubyBuildpackError::BundleInstallCommandError)?;
+
+    Ok(env)
+}
--- buildpacks/ruby/src/steps/default_env.rs
+++ buildpacks/ruby/src/steps/default_env.rs
@@ -4,37 +4,32 @@ use crate::{
 };
 use libcnb::{build::BuildContext, data::layer_name, layer_env::Scope, Env};
 
-pub struct DefaultEnv;
+// Set default environment values
+pub(crate) fn set(
+    context: &BuildContext<RubyBuildpack>,
+    platform_env: &Env,
+) -> libcnb::Result<Env, RubyBuildpackError> {
+    // Get system env vars
+    let mut env = Env::from_current();
 
-impl DefaultEnv {
-    pub fn call(
-        context: &BuildContext<RubyBuildpack>,
-        platform_env: &Env,
-    ) -> libcnb::Result<Env, RubyBuildpackError> {
-        // Get system env vars
-        let mut env = Env::from_current();
-
-        // Apply User env vars
-        // TODO reject harmful vars like GEM_PATH
-        for (k, v) in platform_env {
-            env.insert(k, v);
-        }
-
-        // Setup default environment variables
-        let secret_key_base_layer = context //
-            .handle_layer(
-                layer_name!("secret_key_base"),
-                EnvDefaultsSetSecretKeyBaseLayer,
-            )?;
-        env = secret_key_base_layer.env.apply(Scope::Build, &env);
-
-        let env_defaults_layer = context //
-            .handle_layer(
-                layer_name!("env_defaults"),
-                EnvDefaultsSetStaticVarsLayer,
-            )?;
-        env = env_defaults_layer.env.apply(Scope::Build, &env);
-
-        Ok(env)
+    // Apply User env vars
+    // TODO reject harmful vars like GEM_PATH
+    for (k, v) in platform_env {
+        env.insert(k, v);
     }
+
+    // Setup default environment variables
+    let secret_key_base_layer = context //
+        .handle_layer(
+            layer_name!("secret_key_base"),
+            EnvDefaultsSetSecretKeyBaseLayer,
+        )?;
+    env = secret_key_base_layer.env.apply(Scope::Build, &env);
+
+    let env_defaults_layer =
+        context //
+            .handle_layer(layer_name!("env_defaults"), EnvDefaultsSetStaticVarsLayer)?;
+    env = env_defaults_layer.env.apply(Scope::Build, &env);
+
+    Ok(env)
 }

1f8ab9a761b66bc0505c20193a4d54100bbb742c
Richard Schneeman <rschneeman@salesforce.com>
2022-12-14 14:58:49 -0600

    Move util functions
    The functions in `util` were only being used by one file. I don't love having something that sounds like a junk drawer in my project otherwise it tends to accumulate junk. 
    
    If we need to download other things, then we can extract this logic out to commons and find a better name.
    
    I also flattened the 3 enums so we don't have to map twice in the code. This will have to be undone if we want to extract this logic, but let's build that bridge when we cross it.
    
    Errors need to be revisited across the board. Cleaning this up for the short term.

--- buildpacks/ruby/src/layers/ruby_install_layer.rs
+++ buildpacks/ruby/src/layers/ruby_install_layer.rs
@@ -1,13 +1,15 @@
-use crate::util;
-use crate::util::UrlError;
 use crate::{RubyBuildpack, RubyBuildpackError};
 use commons::gemfile_lock::ResolvedRubyVersion;
+use flate2::read::GzDecoder;
 use libcnb::build::BuildContext;
 use libcnb::data::buildpack::StackId;
 use libcnb::data::layer_content_metadata::LayerTypes;
 use libcnb::layer::{ExistingLayerStrategy, Layer, LayerData, LayerResult, LayerResultBuilder};
 use serde::{Deserialize, Serialize};
+use std::fs;
+use std::io;
 use std::path::Path;
+use tar::Archive;
 use tempfile::NamedTempFile;
 use url::Url;
 
@@ -54,16 +56,17 @@ impl Layer for RubyInstallLayer {
     ) -> Result<LayerResult<Self::Metadata>, RubyBuildpackError> {
         println!("---> Download and extracting Ruby {}", &self.version);
 
-        let tmp_ruby_tgz =
-            NamedTempFile::new().map_err(RubyBuildpackError::CouldNotCreateTemporaryFile)?;
+        let tmp_ruby_tgz = NamedTempFile::new()
+            .map_err(RubyInstallError::CouldNotCreateDestinationFile)
+            .map_err(RubyBuildpackError::RubyInstallError)?;
 
         let url = RubyInstallLayer::download_url(&context.stack_id, &self.version)
-            .map_err(RubyBuildpackError::UrlParseError)?;
+            .map_err(RubyBuildpackError::RubyInstallError)?;
 
-        util::download(url.as_ref(), tmp_ruby_tgz.path())
-            .map_err(RubyBuildpackError::RubyDownloadError)?;
+        download(url.as_ref(), tmp_ruby_tgz.path())
+            .map_err(RubyBuildpackError::RubyInstallError)?;
 
-        util::untar(tmp_ruby_tgz.path(), layer_path).map_err(RubyBuildpackError::RubyUntarError)?;
+        untar(tmp_ruby_tgz.path(), layer_path).map_err(RubyBuildpackError::RubyInstallError)?;
 
         LayerResultBuilder::new(RubyInstallLayerMetadata {
             version: self.version.to_string(),
@@ -99,13 +102,16 @@ impl Layer for RubyInstallLayer {
 }
 
 impl RubyInstallLayer {
-    fn download_url(stack: &StackId, version: impl std::fmt::Display) -> Result<Url, UrlError> {
+    fn download_url(
+        stack: &StackId,
+        version: impl std::fmt::Display,
+    ) -> Result<Url, RubyInstallError> {
         let filename = format!("ruby-{}.tgz", version);
         let base = "https://heroku-buildpack-ruby.s3.us-east-1.amazonaws.com";
-        let mut url = Url::parse(base).map_err(UrlError::UrlParseError)?;
+        let mut url = Url::parse(base).map_err(RubyInstallError::UrlParseError)?;
 
         url.path_segments_mut()
-            .map_err(|_| UrlError::InvalidBaseUrl(String::from(base)))?
+            .map_err(|_| RubyInstallError::InvalidBaseUrl(String::from(base)))?
             .push(stack)
             .push(&filename);
         Ok(url)
@@ -126,4 +132,57 @@ mod tests {
             "https://heroku-buildpack-ruby.s3.us-east-1.amazonaws.com/heroku-20/ruby-2.7.4.tgz",
         );
     }
+}
+
+pub(crate) fn download(
+    uri: impl AsRef<str>,
+    destination: impl AsRef<Path>,
+) -> Result<(), RubyInstallError> {
+    let mut response_reader = ureq::get(uri.as_ref())
+        .call()
+        .map_err(|err| RubyInstallError::RequestError(Box::new(err)))?
+        .into_reader();
+
+    let mut destination_file = fs::File::create(destination.as_ref())
+        .map_err(RubyInstallError::CouldNotCreateDestinationFile)?;
+
+    io::copy(&mut response_reader, &mut destination_file)
+        .map_err(RubyInstallError::CouldNotWriteDestinationFile)?;
+
+    Ok(())
+}
+
+pub(crate) fn untar(
+    path: impl AsRef<Path>,
+    destination: impl AsRef<Path>,
+) -> Result<(), RubyInstallError> {
+    let file = fs::File::open(path.as_ref()).map_err(RubyInstallError::CouldNotOpenFile)?;
+
+    Archive::new(GzDecoder::new(file))
+        .unpack(destination.as_ref())
+        .map_err(RubyInstallError::CouldNotUnpack)
+}
+
+#[derive(thiserror::Error, Debug)]
+pub(crate) enum RubyInstallError {
+    #[error("Could not parse url {0}")]
+    UrlParseError(url::ParseError),
+
+    #[error("Invalid base url {0}")]
+    InvalidBaseUrl(String),
+
+    #[error("Could not open file: {0}")]
+    CouldNotOpenFile(std::io::Error),
+    #[error("Could not untar: {0}")]
+    CouldNotUnpack(std::io::Error),
+
+    // Boxed to prevent `large_enum_variant` errors since `ureq::Error` is massive.
+    #[error("Download error: {0}")]
+    RequestError(Box<ureq::Error>),
+
+    #[error("Could not create file: {0}")]
+    CouldNotCreateDestinationFile(std::io::Error),
+
+    #[error("Could not write file: {0}")]
+    CouldNotWriteDestinationFile(std::io::Error),
 }

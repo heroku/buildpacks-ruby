5e639e333349af588e09dc1fba05c0dd43780494
Richard Schneeman <rschneeman@salesforce.com>
2023-01-10 10:51:46 -0600

    Move SECRET_KEY_BASE to store.toml
    The metadata store in individual layers is not considered a durable store, it is a cache and the application should behave correctly when it is missing. 
    
    If SECRET_KEY_BASE changes, then all visitors to the website will be logged off. Which is not great.
    
    This change moves the default generated value to `store.toml` which is the intended durable store and should have stronger guarantees for persisting between deploys.

--- buildpacks/ruby/src/layers/env_secret_key_base_layer.rs
+++ buildpacks/ruby/src/layers/env_secret_key_base_layer.rs
@@ -2,13 +2,14 @@ use crate::RubyBuildpack;
 use crate::RubyBuildpackError;
 use libcnb::build::BuildContext;
 use libcnb::data::layer_content_metadata::LayerTypes;
-use libcnb::layer::{ExistingLayerStrategy, Layer, LayerData, LayerResult, LayerResultBuilder};
+use libcnb::generic::GenericMetadata;
+use libcnb::layer::{Layer, LayerResult, LayerResultBuilder};
 use libcnb::layer_env::{LayerEnv, ModificationBehavior, Scope};
-use rand::Rng;
-use serde::{Deserialize, Serialize};
 use std::path::Path;
 
-pub(crate) struct EnvSecretKeyBaseLayer;
+pub(crate) struct EnvSecretKeyBaseLayer {
+    pub default_value: String,
+}
 
 /// # Set the `SECRET_KEY_BASE` environment variable
 ///
@@ -18,20 +19,15 @@ pub(crate) struct EnvSecretKeyBaseLayer;
 ///
 /// Must run before any `rake` or `rails` commands are executed as the application may
 /// error without this environment variable.
-#[derive(Deserialize, Serialize, Debug, Clone)]
-pub(crate) struct EnvSecretKeyBaseLayerMetadata {
-    default_value: String,
-}
-
 impl Layer for EnvSecretKeyBaseLayer {
     type Buildpack = RubyBuildpack;
-    type Metadata = EnvSecretKeyBaseLayerMetadata;
+    type Metadata = GenericMetadata;
 
     fn types(&self) -> LayerTypes {
         LayerTypes {
             build: true,
             launch: true,
-            cache: true,
+            cache: false,
         }
     }
 
@@ -40,28 +36,13 @@ impl Layer for EnvSecretKeyBaseLayer {
         _context: &BuildContext<Self::Buildpack>,
         _layer_path: &Path,
     ) -> Result<LayerResult<Self::Metadata>, RubyBuildpackError> {
-        let mut rng = rand::thread_rng();
-        let secret_key_base_default: String = (0..64)
-            .map(|_| rng.sample(rand::distributions::Alphanumeric) as char)
-            .collect();
-
-        LayerResultBuilder::new(Self::Metadata {
-            default_value: secret_key_base_default.clone(),
-        })
-        .env(LayerEnv::new().chainable_insert(
-            Scope::All,
-            ModificationBehavior::Default,
-            "SECRET_KEY_BASE",
-            secret_key_base_default,
-        ))
-        .build()
-    }
-
-    fn existing_layer_strategy(
-        &self,
-        _context: &BuildContext<Self::Buildpack>,
-        _layer: &LayerData<Self::Metadata>,
-    ) -> Result<ExistingLayerStrategy, RubyBuildpackError> {
-        Ok(ExistingLayerStrategy::Keep)
+        LayerResultBuilder::new(Self::Metadata::default())
+            .env(LayerEnv::new().chainable_insert(
+                Scope::All,
+                ModificationBehavior::Default,
+                "SECRET_KEY_BASE",
+                self.default_value.clone(),
+            ))
+            .build()
     }
 }

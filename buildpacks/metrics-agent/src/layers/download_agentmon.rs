439d2660e06f10b7be0058bd3cd77940182a2dd8
Richard Schneeman <rschneeman@salesforce.com>
2023-07-13 15:29:26 -0700

    Introduce new metrics buildpack
    The classic `heroku/metrics` buildpack sets up a statsd daemon that the dyno can use to send Heroku language specific metrics. For Ruby the biggest one is Puma Pool Capacity.
    
    This code will download the metrics client and put it on the path, however there's an issue affecting our ability to actually boot a daemon.
    
    ## Context
    
    Classic buildpacks (Heroku v2) have acces to a `profile.d` directory they can use to write scripts which execute at boot time. This was largely needed to set build time environment variables because the directory structure of build and runtime was different. It was eventually used by the `heroku/metrics` buildpack to download an run a metrics daemon in the background.
    
    Cloud Native Buildpacks (CNB) removed support for bash in an effort to simplify the spec and allow buildpacks to target extremely lightweight distributions that do not include `bash` (for example if you are running a pure JVM application, then there's no runtime need to have bash on the system, removing it reduces the image size and can be viewed as a reduction in security surface area).
    
    As part of this work to remove bash from the spec the `profile.d`-like interface was removed from the CNB spec.
    
    ## Problem
    
    Without a spec supported way to execute a script at startup there is no way for a buildpack to provide a background daemon.
    
    ## Possible solutions
    
    - TBD

---
+++ buildpacks/metrics-agent/src/layers/download_agentmon.rs
@@ -0,0 +1,199 @@
+use crate::{MetricsAgentBuildpack, MetricsAgentError};
+use libcnb::{
+    generic::GenericMetadata,
+    layer::{Layer, LayerResultBuilder},
+};
+use std::os::unix::fs::PermissionsExt;
+use url::Url;
+
+use crate::build_output;
+use flate2::read::GzDecoder;
+use libcnb::data::layer_content_metadata::LayerTypes;
+use std::path::Path;
+use tar::Archive;
+use tempfile::NamedTempFile;
+// use url::Url;
+
+#[derive(Debug)]
+pub(crate) struct DownloadAgentmon {
+    pub(crate) section: build_output::Section,
+}
+
+#[derive(thiserror::Error, Debug)]
+pub(crate) enum DownloadAgentmonError {
+    #[error("Could not read file permissions {0}")]
+    PermissionError(std::io::Error),
+
+    #[error("Could not parse url {0}")]
+    UrlParseError(url::ParseError),
+
+    #[error("Could not open file: {0}")]
+    CouldNotOpenFile(std::io::Error),
+
+    #[error("Could not untar: {0}")]
+    CouldNotUnpack(std::io::Error),
+
+    // Boxed to prevent `large_enum_variant` errors since `ureq::Error` is massive.
+    #[error("Download error: {0}")]
+    RequestError(Box<ureq::Error>),
+
+    #[error("Could not create file: {0}")]
+    CouldNotCreateDestinationFile(std::io::Error),
+
+    #[error("Could not write file: {0}")]
+    CouldNotWriteDestinationFile(std::io::Error),
+}
+
+impl Layer for DownloadAgentmon {
+    type Buildpack = MetricsAgentBuildpack;
+    type Metadata = GenericMetadata;
+
+    fn types(&self) -> libcnb::data::layer_content_metadata::LayerTypes {
+        LayerTypes {
+            build: true,
+            launch: true,
+            cache: true,
+        }
+    }
+
+    fn create(
+        &self,
+        _context: &libcnb::build::BuildContext<Self::Buildpack>,
+        layer_path: &std::path::Path,
+    ) -> Result<
+        libcnb::layer::LayerResult<Self::Metadata>,
+        <Self::Buildpack as libcnb::Buildpack>::Error,
+    > {
+        let mut timer = self.section.say_with_inline_timer("Installing");
+
+        let agentmon_tgz = NamedTempFile::new()
+            .map_err(DownloadAgentmonError::CouldNotCreateDestinationFile)
+            .map_err(MetricsAgentError::DownloadAgentmonError)?;
+
+        let url = Url::parse("https://agentmon-releases.s3.amazonaws.com/latest")
+            .map_err(DownloadAgentmonError::UrlParseError)
+            .map_err(MetricsAgentError::DownloadAgentmonError)?;
+
+        download(url.as_ref(), agentmon_tgz.path())
+            .map_err(MetricsAgentError::DownloadAgentmonError)?;
+
+        let destination = layer_path.join("bin");
+        untar(agentmon_tgz.path(), &destination)
+            .map_err(MetricsAgentError::DownloadAgentmonError)?;
+
+        chmod_plus_x(&destination.join("agentmon"))
+            .map_err(DownloadAgentmonError::PermissionError)
+            .map_err(MetricsAgentError::DownloadAgentmonError)?;
+
+        timer.done();
+
+        LayerResultBuilder::new(GenericMetadata::default()).build()
+    }
+
+    fn existing_layer_strategy(
+        &self,
+        _context: &libcnb::build::BuildContext<Self::Buildpack>,
+        _layer_data: &libcnb::layer::LayerData<Self::Metadata>,
+    ) -> Result<libcnb::layer::ExistingLayerStrategy, <Self::Buildpack as libcnb::Buildpack>::Error>
+    {
+        // TODO caching logic
+        //
+        // The classic buildpack actually downloads this binary on dyno boot every time
+        // We could use content headers to check if it needs to be re-downloaded.
+        Ok(libcnb::layer::ExistingLayerStrategy::Recreate)
+    }
+
+    fn migrate_incompatible_metadata(
+        &self,
+        _context: &libcnb::build::BuildContext<Self::Buildpack>,
+        _metadata: &GenericMetadata,
+    ) -> Result<
+        libcnb::layer::MetadataMigration<Self::Metadata>,
+        <Self::Buildpack as libcnb::Buildpack>::Error,
+    > {
+        self.section
+            .say_with_details("Clearing cache", "invalid metadata");
+
+        Ok(libcnb::layer::MetadataMigration::RecreateLayer)
+    }
+}
+
+pub(crate) fn untar(
+    path: impl AsRef<Path>,
+    destination: impl AsRef<Path>,
+) -> Result<(), DownloadAgentmonError> {
+    let file =
+        fs_err::File::open(path.as_ref()).map_err(DownloadAgentmonError::CouldNotOpenFile)?;
+
+    Archive::new(GzDecoder::new(file))
+        .unpack(destination.as_ref())
+        .map_err(DownloadAgentmonError::CouldNotUnpack)
+}
+
+/// Sets file permissions on the given path to 7xx (similar to `chmod +x <path>`)
+fn chmod_plus_x(path: &Path) -> Result<(), std::io::Error> {
+    let mut perms = fs_err::metadata(path)?.permissions();
+    let mut mode = perms.mode();
+    octal_executable_permission(&mut mode);
+    perms.set_mode(mode);
+
+    fs_err::set_permissions(path, perms)
+}
+
+/// Ensures the provided octal number's executable
+/// bit is enabled.
+///
+/// i.e. chmod +x will ensure that the first digit
+/// of the file permission is 7 on unix so if you pass
+/// in 0o455 it would be mutated to 0o755
+fn octal_executable_permission(mode: &mut u32) {
+    *mode |= 0o700;
+}
+
+pub(crate) fn download(
+    uri: impl AsRef<str>,
+    destination: impl AsRef<Path>,
+) -> Result<(), DownloadAgentmonError> {
+    let mut response_reader = ureq::get(uri.as_ref())
+        .call()
+        .map_err(|err| DownloadAgentmonError::RequestError(Box::new(err)))?
+        .into_reader();
+
+    let mut destination_file = fs_err::File::create(destination.as_ref())
+        .map_err(DownloadAgentmonError::CouldNotCreateDestinationFile)?;
+
+    std::io::copy(&mut response_reader, &mut destination_file)
+        .map_err(DownloadAgentmonError::CouldNotWriteDestinationFile)?;
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_chmod() {
+        let tmp = tempfile::tempdir().unwrap();
+        let file = tmp.into_path().join("file");
+        std::fs::write(&file, "lol").unwrap();
+
+        let before = file.metadata().unwrap().permissions().mode();
+        chmod_plus_x(&file).unwrap();
+        let after = file.metadata().unwrap().permissions().mode();
+        assert!(before != after);
+    }
+
+    #[test]
+    fn test_executable_logic() {
+        // Sets executable bit
+        let mut mode = 0o455;
+        octal_executable_permission(&mut mode);
+        assert_eq!(0o755, mode);
+
+        // Does not affect already executable
+        let mut mode = 0o745;
+        octal_executable_permission(&mut mode);
+        assert_eq!(0o745, mode);
+    }
+}
---
+++ buildpacks/metrics-agent/src/main.rs
@@ -0,0 +1,130 @@
+mod layers;
+
+use crate::layers::download_agentmon::{DownloadAgentmon, DownloadAgentmonError};
+use commons::build_output::{self, fmt::ErrorInfo};
+use indoc::formatdoc;
+use libcnb::{
+    build::{BuildContext, BuildResult, BuildResultBuilder},
+    buildpack_main,
+    data::{build_plan::BuildPlanBuilder, layer_name},
+    detect::{DetectContext, DetectResult, DetectResultBuilder},
+    Buildpack,
+};
+
+#[derive(Debug)]
+enum MetricsAgentError {
+    DownloadAgentmonError(DownloadAgentmonError),
+}
+
+impl From<MetricsAgentError> for libcnb::Error<MetricsAgentError> {
+    fn from(error: MetricsAgentError) -> Self {
+        libcnb::Error::BuildpackError(error)
+    }
+}
+
+buildpack_main!(MetricsAgentBuildpack);
+
+pub(crate) struct MetricsAgentBuildpack;
+
+impl Buildpack for MetricsAgentBuildpack {
+    type Platform = libcnb::generic::GenericPlatform;
+    type Metadata = libcnb::generic::GenericMetadata;
+    type Error = MetricsAgentError;
+
+    fn detect(&self, context: DetectContext<Self>) -> libcnb::Result<DetectResult, Self::Error> {
+        let plan_builder = BuildPlanBuilder::new().provides("heroku-statsd-metrics-agent");
+
+        if let Ok(true) = fs_err::read_to_string(context.app_dir.join("Gemfile.lock"))
+            .map(|lockfile| lockfile.contains("barnes"))
+        {
+            DetectResultBuilder::pass()
+                .build_plan(plan_builder.requires("heroku-statsd-metrics-agent").build())
+                .build()
+        } else {
+            DetectResultBuilder::pass()
+                .build_plan(plan_builder.build())
+                .build()
+        }
+    }
+
+    fn build(&self, context: BuildContext<Self>) -> libcnb::Result<BuildResult, Self::Error> {
+        let build_duration = build_output::buildpack_name("Heroku Statsd Metrics Agent");
+
+        let section = build_output::section("Metrics agent");
+        context.handle_layer(
+            layer_name!("statsd-metrics-agent"),
+            DownloadAgentmon { section },
+        )?;
+
+        // TODO write launch script
+        //
+        // if [[ "${AGENTMON_DEBUG}" = "true" ]]; then
+        //     AGENTMON_FLAGS+=("-debug")
+        // fi
+
+        // if [[ -x "${BUILD_DIR}/bin/agentmon" ]]; then
+        //     (while true; do
+        //         ${BUILD_DIR}/bin/agentmon "${AGENTMON_FLAGS[@]}" "${HEROKU_METRICS_URL}"
+        //         echo "agentmon completed with status=${?}. Restarting"
+        //         sleep 1
+        //     done) &
+        // else
+        //     echo "No agentmon executable found. Not starting."
+        // fi
+
+        build_duration.done();
+        BuildResultBuilder::new().build()
+    }
+
+    fn on_error(&self, err: libcnb::Error<Self::Error>) {
+        on_error(err);
+    }
+}
+
+#[derive(Debug)]
+enum Cause {
+    OurError(MetricsAgentError),
+    FrameworkError(libcnb::Error<MetricsAgentError>),
+}
+
+fn cause(err: libcnb::Error<MetricsAgentError>) -> Cause {
+    match err {
+        libcnb::Error::BuildpackError(err) => Cause::OurError(err),
+        err => Cause::FrameworkError(err),
+    }
+}
+
+pub(crate) fn on_error(err: libcnb::Error<MetricsAgentError>) {
+    match cause(err) {
+        Cause::OurError(error) => log_our_error(error),
+        Cause::FrameworkError(error) => ErrorInfo::header_body_details(
+            "heroku/buildpack-ruby internal buildpack error",
+            formatdoc! {"
+                An unexpected internal error was reported by the framework used
+                by this buildpack.
+
+                If the issue persists, consider opening an issue on the GitHub
+                repository. If you are unable to deploy to Heroku as a result
+                of this issue, consider opening a ticket for additional support.
+            "},
+            error,
+        )
+        .print(),
+    };
+}
+
+fn log_our_error(error: MetricsAgentError) {
+    match error {
+        MetricsAgentError::DownloadAgentmonError(error) => ErrorInfo::header_body_details(
+            formatdoc! {
+                "Could not install Statsd agent"
+            },
+            formatdoc! {
+                "An error occured while downloading and installing the metrics agent
+                the buildpack cannot continue"
+            },
+            error,
+        )
+        .print(),
+    }
+}

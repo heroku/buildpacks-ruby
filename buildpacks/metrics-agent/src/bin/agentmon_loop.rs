8528b0b5d91d1100f72cd82283e19c75e08d644f
Richard Schneeman <rschneeman@salesforce.com>
2023-08-10 16:31:21 -0500

    Update agentmon_loop
    - Fix bad docstrings
    - Move sleep_for to a constant
    - Refactor logic into smaller, more consistent functions
    - Added tests for argument generation

--- buildpacks/metrics-agent/src/bin/agentmon_loop.rs
+++ buildpacks/metrics-agent/src/bin/agentmon_loop.rs
@@ -3,77 +3,171 @@
 #![warn(clippy::pedantic)]
 
 use clap::Parser;
-use commons::fun_run::CmdMapExt;
+use std::ffi::OsStr;
+use std::process::ExitStatus;
 use std::{
-    ffi::OsString,
+    collections::HashMap,
     path::{Path, PathBuf},
     process::{exit, Command},
+    thread::sleep,
     time::Duration,
 };
+const SLEEP_FOR: Duration = Duration::from_secs(1);
 
-/// Simple program to greet a person
+/// Agentmon Loop
+///
+/// Boots agentmon (a statsd server) in a loop
+///
+/// Example:
+///
+/// $ cargo run --bin agentmon_loop -- --path <path/to/agentmon/binary>
+
+/// Turn CLI arguments into a Rust struct
 #[derive(Parser, Debug)]
 struct Args {
-    /// Name of the person to greet
+    /// Path to the agentmon executable e.g. --path <path/to/agenmon/binary>
     #[arg(short, long)]
     path: PathBuf,
 }
 
 fn main() {
-    let sleep_for = std::time::Duration::from_secs(1);
-    let heroku_metrics_url = if let Some(url) = std::env::var_os("HEROKU_METRICS_URL") {
-        url
-    } else {
-        eprintln!("Metrics agent exiting: 0 (HEROKU_METRICS_URL is not set)");
-        exit(0)
-    };
-
-    if let Some(true) =
-        std::env::var_os("DYNO").map(|value| value.to_string_lossy().starts_with("run."))
-    {
-        eprintln!("Metrics agent exiting: 0 (one off dyno detected i.e. DYNO=\"run.*\")");
-        exit(0)
-    }
-
     let agentmon = Args::parse().path;
     if !agentmon.exists() {
         eprintln!("Path does not exist {}", agentmon.display());
         exit(1);
     }
 
+    let agentmon_args = match build_args(std::env::vars().collect::<HashMap<String, String>>()) {
+        Ok(args) => args,
+        Err(e) => {
+            eprintln!("Cannot start agentmon: {e}");
+            exit(1)
+        }
+    };
+
     loop {
-        run_agentmon(&agentmon, &heroku_metrics_url, &sleep_for);
+        match run(&agentmon, &agentmon_args) {
+            Ok(status) => {
+                eprintln!("process completed with status=${status}, sleeping {SLEEP_FOR:?}");
+            }
+            Err(error) => {
+                eprintln!("process could not be run due to error: {error}, sleeping {SLEEP_FOR:?}");
+            }
+        };
+        sleep(SLEEP_FOR);
     }
 }
 
-fn run_agentmon(agentmon: &Path, heroku_metrics_url: &OsString, sleep_for: &Duration) {
-    if let Some(port) = std::env::var_os("PORT") {
-        let statsd_addr = {
-            let mut string = OsString::from("statsd-addr=:");
-            string.push(&port);
-            string
-        };
+/// Print and run executable
+///
+/// Runs an executable at the given path with args and streams the results.
+fn run<I, S>(path: &Path, args: I) -> Result<ExitStatus, std::io::Error>
+where
+    I: IntoIterator<Item = S>,
+    S: AsRef<OsStr>,
+{
+    let mut cmd = Command::new(path);
+    cmd.args(args);
 
-        let result = Command::new(agentmon).cmd_map(|cmd| {
-            cmd.arg(&statsd_addr);
+    eprintln!("Running: {}", commons::fun_run::display(&mut cmd));
 
-            if let Some(true) = std::env::var_os("AGENTMON_DEBUG").map(|value| value == *"true") {
-                cmd.arg("-debug");
-            };
-            cmd.arg(heroku_metrics_url);
+    cmd.spawn().and_then(|mut child| child.wait())
+}
 
-            cmd.spawn().and_then(|mut child| child.wait())
-        });
+#[derive(Debug, thiserror::Error, PartialEq)]
+enum Error {
+    #[error("PORT environment variable is not set")]
+    MissingPort,
 
-        match result {
-            Ok(status) => eprintln!("agentmon completed with status=${status}. Restarting"),
-            Err(error) => {
-                eprintln!("agentmon could not be run due to error: {error}");
-                eprintln!("Retrying");
-            }
-        };
-    } else {
-        eprintln!("PORT is not set, sleeping {sleep_for:?}");
+    #[error("HEROKU_METRICS_URL environment variable is not set")]
+    MissingMetricsUrl,
+
+    #[error("One off dyno detected i.e. DYNO=\"run.*\"")]
+    RunDynoDetected,
+}
+
+/// Constructs the arguments for agentmon based on environment variables
+///
+/// # Errors
+///
+/// - PORT is not set
+/// - HEROKU_METRICS_URL is not set
+/// - DYNO starts with `run.`
+fn build_args(env: HashMap<String, String>) -> Result<Vec<String>, Error> {
+    let mut args = Vec::new();
+    if let Some(true) = env.get("DYNO").map(|value| value.starts_with("run.")) {
+        return Err(Error::RunDynoDetected);
     }
-    std::thread::sleep(*sleep_for);
+
+    if let Some(port) = env.get("PORT") {
+        args.push(format!("statsd-addr=:{port}"));
+    } else {
+        return Err(Error::MissingPort);
+    };
+
+    if let Some(true) = env.get("AGENTMON_DEBUG").map(|value| value == "true") {
+        args.push("-debug".to_string());
+    };
+
+    if let Some(url) = env.get("HEROKU_METRICS_URL") {
+        args.push(url.clone());
+    } else {
+        return Err(Error::MissingMetricsUrl);
+    };
+
+    Ok(args)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn missing_port() {
+        let result = build_args(HashMap::new());
+
+        assert_eq!(result, Err(Error::MissingPort));
+    }
+
+    #[test]
+    fn agentmon_args() {
+        let mut env = HashMap::new();
+        env.insert("PORT".to_string(), "90210".to_string());
+        env.insert(
+            "HEROKU_METRICS_URL".to_string(),
+            "https://example.com".to_string(),
+        );
+
+        let result = build_args(env);
+
+        assert_eq!(
+            result,
+            Ok(vec![
+                "statsd-addr=:90210".to_string(),
+                "https://example.com".to_string()
+            ])
+        );
+    }
+
+    #[test]
+    fn agentmon_debug_args() {
+        let mut env = HashMap::new();
+        env.insert("PORT".to_string(), "90210".to_string());
+        env.insert(
+            "HEROKU_METRICS_URL".to_string(),
+            "https://example.com".to_string(),
+        );
+        env.insert("AGENTMON_DEBUG".to_string(), "true".to_string());
+
+        let result = build_args(env);
+
+        assert_eq!(
+            result,
+            Ok(vec![
+                "statsd-addr=:90210".to_string(),
+                "-debug".to_string(),
+                "https://example.com".to_string()
+            ])
+        );
+    }
 }

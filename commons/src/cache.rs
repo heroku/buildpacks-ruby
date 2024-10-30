d5fa831f75cc7cdf66599ebff7520c1be4cb70be
Schneems <richard.schneeman+foo@gmail.com>
2024-10-30 13:45:13 -0500

    Remove previously deprecated interface

--- commons/src/cache.rs
+++ commons/src/cache.rs
@@ -1,13 +1,10 @@
 mod app_cache;
-mod app_cache_collection;
 mod clean;
 mod config;
 mod error;
 
 pub use self::app_cache::{build, PathState};
 pub use self::app_cache::{AppCache, CacheState};
-#[allow(deprecated)]
-pub use self::app_cache_collection::AppCacheCollection;
 pub use self::clean::FilesWithSize;
 pub use self::config::CacheConfig;
 pub use self::config::{mib, KeepPath};

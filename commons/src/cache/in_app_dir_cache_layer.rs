cff989b10d063de3106ad01edf59f51804e71a02
Schneems <richard.schneeman+foo@gmail.com>
2024-10-30 13:40:41 -0500

    Remove unused code

--- commons/src/cache/in_app_dir_cache_layer.rs
+++ commons/src/cache/in_app_dir_cache_layer.rs
@@ -1,11 +1,4 @@
-use libcnb::build::BuildContext;
-use libcnb::data::layer_content_metadata::LayerTypes;
-#[allow(deprecated)]
-use libcnb::layer::{ExistingLayerStrategy, Layer, LayerData, LayerResult, LayerResultBuilder};
-use libcnb::Buildpack;
 use serde::{Deserialize, Serialize};
-use std::marker::PhantomData;
-use std::path::Path;
 use std::path::PathBuf;
 
 /// # Caches a folder in the application directory
@@ -22,62 +15,7 @@ use std::path::PathBuf;
 ///  Historically, sprockets will keep 3 versions of old files on disk. This
 ///  allows for emails, that might live a long time, to reference a specific SHA of an
 ///  asset.
-#[derive(Deserialize, Serialize, Debug, Clone)]
-pub(crate) struct InAppDirCacheLayer<B> {
-    pub(crate) app_dir_path: PathBuf,
-    buildpack: PhantomData<B>,
-}
-
 #[derive(Deserialize, Serialize, Debug, Clone, PartialEq, Eq)]
 pub(crate) struct Metadata {
     pub(crate) app_dir_path: PathBuf,
-}
-
-impl<B> InAppDirCacheLayer<B> {
-    pub(crate) fn new(app_dir_path: PathBuf) -> Self {
-        Self {
-            app_dir_path,
-            buildpack: PhantomData,
-        }
-    }
-}
-
-#[allow(deprecated)]
-impl<B> Layer for InAppDirCacheLayer<B>
-where
-    B: Buildpack,
-{
-    type Buildpack = B;
-    type Metadata = Metadata;
-
-    fn types(&self) -> LayerTypes {
-        LayerTypes {
-            build: true,
-            launch: true,
-            cache: true,
-        }
-    }
-
-    fn create(
-        &mut self,
-        _context: &BuildContext<Self::Buildpack>,
-        _layer_path: &Path,
-    ) -> Result<LayerResult<Self::Metadata>, B::Error> {
-        LayerResultBuilder::new(Metadata {
-            app_dir_path: self.app_dir_path.clone(),
-        })
-        .build()
-    }
-
-    fn existing_layer_strategy(
-        &mut self,
-        _context: &BuildContext<Self::Buildpack>,
-        layer_data: &LayerData<Self::Metadata>,
-    ) -> Result<ExistingLayerStrategy, B::Error> {
-        if self.app_dir_path == layer_data.content_metadata.metadata.app_dir_path {
-            Ok(ExistingLayerStrategy::Keep)
-        } else {
-            Ok(ExistingLayerStrategy::Recreate)
-        }
-    }
 }

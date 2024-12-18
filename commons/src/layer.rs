8e5134b4e378db41960cb3f6bf731f85d174bc2b
Schneems <richard.schneeman+foo@gmail.com>
2024-12-18 11:39:11 -0600

    Make deprecation more specific

--- commons/src/layer.rs
+++ commons/src/layer.rs
@@ -1,5 +1,14 @@
 mod configure_env_layer;
 mod default_env_layer;
 
+#[deprecated(
+    since = "0.0.0",
+    note = "Use the struct layer API in the latest libcnb.rs instead"
+)]
 pub use self::configure_env_layer::ConfigureEnvLayer;
+
+#[deprecated(
+    since = "0.0.0",
+    note = "Use the struct layer API in the latest libcnb.rs instead"
+)]
 pub use self::default_env_layer::DefaultEnvLayer;

09b7533914f73d9894ebc862af5053e995d3ebad
Schneems <richard.schneeman+foo@gmail.com>
2024-12-18 12:09:56 -0600

    Add testing helper to cache_buddy

--- commons/src/layer/cache_buddy.rs
+++ commons/src/layer/cache_buddy.rs
@@ -136,6 +136,50 @@ impl<M> AsRef<str> for Meta<M> {
     }
 }
 
+/// Takes in a directory and returns a minimal build context for use in testing caching behavior
+#[cfg(test)]
+fn temp_build_context<B: libcnb::Buildpack>(
+    from_dir: impl AsRef<std::path::Path>,
+    buildpack_toml_string: &str,
+) -> libcnb::build::BuildContext<B> {
+    let base_dir = from_dir.as_ref().to_path_buf();
+    let layers_dir = base_dir.join("layers");
+    let app_dir = base_dir.join("app_dir");
+    let platform_dir = base_dir.join("platform_dir");
+    let buildpack_dir = base_dir.join("buildpack_dir");
+    for dir in [&app_dir, &layers_dir, &buildpack_dir, &platform_dir] {
+        std::fs::create_dir_all(dir).unwrap();
+    }
+
+    let target = libcnb::Target {
+        os: String::new(),
+        arch: String::new(),
+        arch_variant: None,
+        distro_name: String::new(),
+        distro_version: String::new(),
+    };
+    let platform =
+        <<B as libcnb::Buildpack>::Platform as libcnb::Platform>::from_path(&platform_dir).unwrap();
+    let buildpack_descriptor: libcnb::data::buildpack::ComponentBuildpackDescriptor<
+        <B as libcnb::Buildpack>::Metadata,
+    > = toml::from_str(buildpack_toml_string).unwrap();
+    let buildpack_plan = libcnb::data::buildpack_plan::BuildpackPlan {
+        entries: Vec::<libcnb::data::buildpack_plan::Entry>::new(),
+    };
+    let store = None;
+
+    libcnb::build::BuildContext {
+        layers_dir,
+        app_dir,
+        buildpack_dir,
+        target,
+        platform,
+        buildpack_plan,
+        buildpack_descriptor,
+        store,
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;

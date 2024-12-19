f5e8ab8e5586acd607b915f1d69975b8aad4e02f
Schneems <richard.schneeman+foo@gmail.com>
2024-12-19 09:53:30 -0600

    Remove unused `since` versions

--- commons/src/layer.rs
+++ commons/src/layer.rs
@@ -2,14 +2,8 @@ pub mod cache_buddy;
 mod configure_env_layer;
 mod default_env_layer;
 
-#[deprecated(
-    since = "0.0.0",
-    note = "Use the struct layer API in the latest libcnb.rs instead"
-)]
+#[deprecated(note = "Use the struct layer API in the latest libcnb.rs instead")]
 pub use self::configure_env_layer::ConfigureEnvLayer;
 
-#[deprecated(
-    since = "0.0.0",
-    note = "Use the struct layer API in the latest libcnb.rs instead"
-)]
+#[deprecated(note = "Use the struct layer API in the latest libcnb.rs instead")]
 pub use self::default_env_layer::DefaultEnvLayer;
